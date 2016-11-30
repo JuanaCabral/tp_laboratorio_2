9e758420491b05d97edbcdbf948c0c1b69dbd1f4
JuanaCabral <juanaester_cabral@hotmail.com>
2016-11-30 00:54:45 -0300

    76

---
+++ Navegador TP-4 - AlumnoV2/Navegador TP-4 - AlumnoV2/Hilo/Descargador.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net; // Avisar del espacio de nombre
+using System.ComponentModel;
+using System.Windows;
+
+
+
+namespace Hilo
+{
+    public class Descargador
+    {
+        private string html;
+        private Uri direccion;
+
+        public Descargador(Uri direccion)
+        {
+            this.html = "";
+            this.direccion = direccion;
+
+        }
+
+        public void IniciarDescarga()
+        {
+            try
+            {
+                WebClient cliente = new WebClient();
+
+                cliente.DownloadProgressChanged += new DownloadProgressChangedEventHandler(WebClientDownloadProgressChanged);
+
+                cliente.DownloadStringCompleted += new DownloadStringCompletedEventHandler(WebClientDownloadCompleted);
+
+                cliente.DownloadStringAsync(direccion);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        private void WebClientDownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+           // DownloadStringCompleted(sender, e)
+          
+        }
+        private void WebClientDownloadCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            DownloadStringCompleted(sender, e);
+            
+        }
+    }
+}
+
