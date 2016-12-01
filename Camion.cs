5b065c24c0b6d0a29e7e14d744d0f0758e769b3d
JuanaCabral <juanaester_cabral@hotmail.com>
2016-12-01 00:05:08 -0300

    Juanatp

---
+++ Camion.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clase_12_Library_2;
+
+namespace Clase_12_Library
+{
+    public class Camion:Vehiculo
+    {
+        public Camion(EMarca marca, string patente, ConsoleColor color)
+            : base( marca, patente,color)
+        {
+        }
+        /// <summary>
+        /// Los camiones tienen 8 ruedas
+        /// </summary>
+        public override short CantidadRuedas
+        {
+            get
+            {
+                return 8;
+            }
+            set { }
+        }
+
+        public override  string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("CAMION");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine("RUEDAS : {0}" + this.CantidadRuedas);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Moto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clase_12_Library_2;
+
+using System.Drawing;
+
+namespace Clase_12_Library
+{
+   public  class Moto:Vehiculo
+    {
+        public Moto(EMarca marca, string patente, ConsoleColor color)
+            : base(marca,patente, color)
+        {
+        }
+        /// <summary>
+        /// Las motos tienen 2 ruedas
+        /// </summary>
+        public override short CantidadRuedas
+        {
+            get
+            {
+                return 2;
+            }
+            set
+            {
+            }
+        }
+
+        public override  string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("MOTO");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine("RUEDAS : {0}"+ this.CantidadRuedas);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
